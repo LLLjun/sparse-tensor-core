689d7261c83f8493d2ecca895a9ecb29f88b65df
SummerDay231 <xiahao231@163.com>
2023-05-02 21:50:36 +0800

    fix syntax errors

--- kernel_verilog/src/unstruct/dn_router.v
+++ kernel_verilog/src/unstruct/dn_router.v
@@ -9,8 +9,8 @@ module dn_router
     input set_en,
     input route_en,
     input [1:0] route_signal,
-    input [DW_DATA-1:0] in [1:0],
-    output [DW_DATA-1:0] out [1:0]
+    input [DW_DATA*2-1:0] in,
+    output [DW_DATA*2-1:0] out
 );
 
     reg [DW_DATA-1:0] reg_out_left, reg_out_right;
@@ -20,16 +20,27 @@ module dn_router
             reg_out_right <= in[1];
         end
         else if (set_en) begin: set_block
-            reg_out_left <= in[route_signal[0]];
-            reg_out_right <= in[route_signal[1]];
+            // set left out
+            if (route_signal[0]) begin
+                reg_out_left <= in[2*DW_DATA-1:DW_DATA];
+            end
+            else begin
+                reg_out_left <= in[DW_DATA-1:0];
+            end
+            // set right out
+            if (route_signal[1]) begin
+                reg_out_right <= in[2*DW_DATA-1:DW_DATA];
+            end
+            else begin
+                reg_out_right <= in[DW_DATA-1:0];
+            end
         end
-        else if (~route_en) begin: 
+        else if (~route_en) begin: route_block
             reg_out_left <= 0;
             reg_out_right <= 0;
         end
     end
 
-    assign out[0] = reg_out_left;
-    assign out[1] = reg_out_right;
+    assign out = {reg_out_left, reg_out_right};
 
 endmodule
