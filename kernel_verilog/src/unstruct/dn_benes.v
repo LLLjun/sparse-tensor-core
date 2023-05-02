8f5a6bed91545e343a050824ea7e6b742afb5a22
SummerDay231 <xiahao231@163.com>
2023-05-02 23:36:58 +0800

    fix syntax errors and pass synthesis, fck verilog!

--- kernel_verilog/src/unstruct/dn_benes.v
+++ kernel_verilog/src/unstruct/dn_benes.v
@@ -10,15 +10,18 @@ module dn_benes
     input reset,
     input set_en,
     input route_en,
-    input [1:0] route_signals [(N_LEVELS-1)*N/2-1:0],
+    input [2*(N_LEVELS-1)*N/2-1:0] route_signals,
     input [DW_DATA*N-1:0] in,
     output [DW_DATA*N-1:0] out
 );
 
-    reg [DW_DATA*N_LEVELS*N-1:0] wire_in;
-    reg [DW_DATA*N_LEVELS*N-1:0] wire_out;
+    reg [DW_DATA*N_LEVELS*N-1:0] reg_in;
+    wire [DW_DATA*N_LEVELS*N-1:0] wire_out;
 
     integer i, j, mid, l, pos1, pos2;
+    
+    // set output connections
+    assign out = wire_out[DW_DATA*N_LEVELS*N-1 -:DW_DATA*N];
 
     always @(*) begin
     // set input connections
@@ -26,15 +29,7 @@ module dn_benes
     for (j=0; j<N; j=j+1) begin
         pos1 = i*N+j;
         pos2 = j;
-        wire_in[(pos1+1)*DW_DATA-1 -:DW_DATA] <= in[(pos2+1)*DW_DATA-1 -:DW_DATA];
-    end
-
-    // set output connections
-    i = N_LEVELS - 1;
-    for (j=0; j<N; j=j+1) begin
-        pos1 = j;
-        pos2 = i*N+j;
-        wire_in[(pos1+1)*DW_DATA-1 -:DW_DATA] <= in[(pos2+1)*DW_DATA-1 -:DW_DATA];
+        reg_in[(pos1+1)*DW_DATA-1 -:DW_DATA] <= in[(pos2+1)*DW_DATA-1 -:DW_DATA];
     end
 
     // set inter connections
@@ -45,23 +40,23 @@ module dn_benes
         for (j=0; j<N; j=j+1) begin
             pos1 = (l+1)*N + j;
             pos2 = l*N + (j+2**i)%2**(i+1) + j/(2**(i+1))*(2**(i+1));
-            wire_in[(pos1+1)*DW_DATA-1 -:DW_DATA] <= in[(pos2+1)*DW_DATA-1 -:DW_DATA];
+            reg_in[(pos1+1)*DW_DATA-1 -:DW_DATA] <= wire_out[(pos2+1)*DW_DATA-1 -:DW_DATA];
         end
         // set the right part
         l = mid + i;
         for (j=0; j<N; j=j+1) begin
             pos1 = (l+1)*N + j;
             pos2 = l*N + (j+2**i)%2**(i+1) + j/(2**(i+1))*(2**(i+1));
-            wire_in[(pos1+1)*DW_DATA-1 -:DW_DATA] <= in[(pos2+1)*DW_DATA-1 -:DW_DATA];
+            reg_in[(pos1+1)*DW_DATA-1 -:DW_DATA] <= wire_out[(pos2+1)*DW_DATA-1 -:DW_DATA];
         end
     end
     end
 
     // generate router layers 
-    genvar x, y;
+    genvar level, pos;
     generate
-        for (x=0; x<N_LEVELS; x=x+1) begin
-            for (y=0; y<N; y=y+1) begin
+        for (level=0; level<N_LEVELS; level=level+1) begin
+            for (pos=0; pos<N/2; pos=pos+1) begin
                 dn_router #(
                     .DW_DATA(DW_DATA)
                 ) u_dn_router(
@@ -70,8 +65,8 @@ module dn_benes
                     .set_en(set_en),
                     .route_en(route_en),
                     .route_signal(route_signals[l*N/2+j]),
-                    .in({wire_in[(x*N+y+1)*DW_DATA-1 -:DW_DATA], wire_in[(x*N+y+2)*DW_DATA-1 -:DW_DATA]}),
-                    .out({wire_out[(x*N+y+1)*DW_DATA-1 -:DW_DATA], wire_out[(x*N+y+2)*DW_DATA-1 -:DW_DATA]})
+                    .in({reg_in[(level*N+2*pos+1)*DW_DATA-1 -:DW_DATA], reg_in[(level*N+2*pos+2)*DW_DATA-1 -:DW_DATA]}),
+                    .out({wire_out[(level*N+2*pos+1)*DW_DATA-1 -:DW_DATA], wire_out[(level*N+2*pos+2)*DW_DATA-1 -:DW_DATA]})
                 );
             end
         end
